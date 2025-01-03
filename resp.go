fb4301567f89e6e3b4600b5ed9f5b161197b8a3c
milagro <millagros77@gmail.com>
2025-01-03 19:03:23 +0300

    Read added

--- resp.go
+++ resp.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bufio"
+	"io"
+	"strconv"
+)
+
 const (
 	STRING  = "+"
 	INTEGER = ":"
@@ -14,4 +20,84 @@ type Value struct {
 	num   int
 	bulk  string
 	array []Value
+}
+
+type Resp struct {
+	reader *bufio.Reader
+}
+
+func (r *Resp) New(reader io.Reader) *Resp {
+	return &Resp{
+		reader: bufio.NewReader(reader),
+	}
+}
+
+func (r *Resp) readLine() (line []byte, n int, err error) {
+	for {
+		b, err := r.reader.ReadByte()
+		if err != nil {
+			return nil, 0, err
+		}
+		n++
+		line = append(line, b)
+		if b == '\r' && line[n-1] == '\n' && len(line) >= 2 {
+			break
+		}
+	}
+	return line, n, nil
+}
+
+func (r *Resp) readInteger() (number int, n int, err error) {
+	line, n, err := r.readLine()
+	if err != nil {
+		return 0, 0, err
+	}
+	_, err = strconv.ParseInt(string(line), 0, 64)
+	if err != nil {
+		return 0, n, err
+	}
+	return number, n, nil
+}
+
+func (r *Resp) readBulk() (Value, error) {
+	var value Value
+	value.typ = "bulk"
+
+	length, _, err := r.readInteger()
+	if err != nil {
+		return Value{}, err
+	}
+
+	bulk := make([]byte, length)
+
+	_, err = r.reader.Read(bulk)
+	if err != nil {
+		return Value{}, err
+	}
+
+	value.bulk = string(bulk)
+
+	r.readLine()
+
+	return value, nil
+}
+
+func (r *Resp) readArray() (Value, error) {
+	var value Value
+	value.typ = "array"
+
+	length, _, err := r.readInteger()
+	if err != nil {
+		return Value{}, err
+	}
+
+	value.array = make([]Value, length)
+	for i := 0; i < length; i++ {
+		val, err := r.Read()
+		if err != nil {
+			return Value{}, err
+		}
+		value.array[i] = val
+	}
+	return value, nil
 }
