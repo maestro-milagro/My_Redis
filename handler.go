ac72d672b4e97cc0d8620a513eac89014ac17d8f
milagro <millagros77@gmail.com>
2025-01-06 15:06:06 +0300

    Set & Get added

--- handler.go
+++ handler.go
@@ -1,9 +1,50 @@
 package main
 
+import "sync"
+
 var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
+	"GET":  get,
+	"SET":  set,
 }
+
+var SetMap map[string]string
+
+var SetMutex sync.RWMutex
 
 func ping(args []Value) Value {
 	return Value{typ: "string", str: "PONG"}
+}
+
+func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "Invalid number of arguments"}
+	}
+
+	key := args[0].bulk
+	value := args[1].bulk
+
+	SetMutex.Lock()
+	SetMap[key] = value
+	SetMutex.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func get(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "Invalid number of arguments"}
+	}
+
+	key := args[0].bulk
+
+	SetMutex.RLock()
+	value, ok := SetMap[key]
+	SetMutex.RUnlock()
+
+	if !ok {
+		return Value{str: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
 }
